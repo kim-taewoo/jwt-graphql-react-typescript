4d7c0adecf363448f22980491d8fc918068c19cc
kim-taewoo <acoustic0419@gmail.com>
2020-10-14 18:20:26 +0900

    [추가] Home page에 graphql 쿼리를 받아오는 예시 코드 추가

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useHelloQuery } from '../generated/graphql';
 
-interface Props {
-  
-}
+interface Props {}
 
 export const Home = (props: Props) => {
+  const { data, loading } = useHelloQuery();
+  if (loading || !data) {
+    return <div>로딩중입니다...</div>
+  }
   return (
-    <div>Home Page</div>
-  )
-}
+    <div>
+      <div>Home Page</div>
+      <div>{data.hello}</div>
+    </div>
+  );
+};
